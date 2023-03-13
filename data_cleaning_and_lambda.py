824bddc454d5c2c6889ecca6c88f11909500390e
manoharmagar <127617617+manoharmagar@users.noreply.github.com>
2023-03-13 09:08:18 +0530

    new line added

--- data_cleaning_and_lambda.py
+++ data_cleaning_and_lambda.py
@@ -10,6 +10,8 @@
 import sys
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
+
+
 from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
 from awsglue.job import Job
